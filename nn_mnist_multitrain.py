1cf89c8226d8f6e5f752f404c0aa58e061b186de
Jose Luis Bracamonte Amavizca <luisjba@gmail.com>
2022-02-23 19:09:09 -0700

    Fixed generate plots and test with one

--- nn_mnist_multitrain.py
+++ nn_mnist_multitrain.py
@@ -13,10 +13,10 @@ EPOCHS=200
 BATCH_SIZE=128
 models_layers_conf = [
     [],                                 # The default, with no hidden layers
-    [[128,'relu']],                     # One hidden layer with 128 units and 'relu' activation
-    [[128,'relu'],[64,'relu']],
-    [[128,'relu'],[128,'relu']],
-    [[128,'relu'],[128,'relu'],[64,'relu']],
+    # [[128,'relu']],                     # One hidden layer with 128 units and 'relu' activation
+    # [[128,'relu'],[64,'relu']],
+    # [[128,'relu'],[128,'relu']],
+    # [[128,'relu'],[128,'relu'],[64,'relu']],
 ]
 
 models_list = []
@@ -52,6 +52,6 @@ fig.tight_layout()
 for i in range(plot_rows):
     offset_index = i * plot_cols
     ax_acc, ax_loss = axs.flat[offset_index:offset_index+2]
-    models_list[i].add_learning_curve_plots(fig, ax_acc, ax_loss)
+    models_list[i]["model"].add_learning_curve_plots(fig, ax_acc, ax_loss)
 # Save the plots
 plt.savefig('graficos_{}.png'.format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S")))
